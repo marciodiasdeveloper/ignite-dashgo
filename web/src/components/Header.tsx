82298ddd11746bad22e5f5e1a44e64ccd24b570b
Your Name <you@example.com>
2021-04-02 12:22:13 -0300

    FIx

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
     return (
@@ -46,6 +46,33 @@ export function Header() {
                 />
 
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+      
+            <Flex
+                align="center"
+                ml="auto"
+
+            >
+                <HStack 
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+        
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Márcio Dias</Text>
+                        <Text color="gray.300" fontSize="small">contato@marciodias.me</Text>
+                    </Box>
+                    <Avatar size="md" name="Márcio Dias" />
+                </Flex>
             </Flex>
         </Flex>
     );
