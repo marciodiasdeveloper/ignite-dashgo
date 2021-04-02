f3cf22cd81c83b4c980e1930b825197158284374
Your Name <you@example.com>
2021-04-02 19:53:45 -0300

    Fix

--- web/src/components/Header/Profile.tsx
+++ web/src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Márcio Dias</Text>
-                <Text color="gray.300" fontSize="small">contato@marciodias.me</Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Márcio Dias</Text>
+                    <Text color="gray.300" fontSize="small">contato@marciodias.me</Text>
+                </Box>
+            )}
             <Avatar size="md" name="Márcio Dias" />
         </Flex>
     );
