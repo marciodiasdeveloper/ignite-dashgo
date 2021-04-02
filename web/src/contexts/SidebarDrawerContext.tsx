0c07bac935e77b806267786cbbdfb6a020d5b0f1
Your Name <you@example.com>
2021-04-02 20:29:19 -0300

    Fix

--- web/src/contexts/SidebarDrawerContext.tsx
+++ web/src/contexts/SidebarDrawerContext.tsx
@@ -1,5 +1,6 @@
 import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/hooks";
-import { createContext, ReactNode, useContext } from "react";
+import { useEffect, createContext, ReactNode, useContext } from "react";
+import { useRouter } from 'next/router';
 
 interface SidebarDrawerProviderProps {
     children: ReactNode;
@@ -11,6 +12,12 @@ const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
 
 export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
     const disclosure = useDisclosure();
+    const router = useRouter();
+
+    useEffect(() => {
+        disclosure.onClose();
+    }, [router.asPath]);
+
 
     return (
         <SidebarDrawerContext.Provider value={disclosure}>
