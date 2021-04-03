b598922b9885918fefe8990011078e3526e785c2
Your Name <you@example.com>
2021-04-02 23:57:40 -0300

    Fix

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,20 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
-import { } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password:string;
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data, event) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -18,12 +30,15 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input type="email" name="email" label="E-mail" />
-          <Input type="password" name="password" label="Senha" />
+          <Input type="email" name="email" label="E-mail" ref={register} />
+          <Input type="password" name="password" label="Senha" ref={register} />
         </Stack>       
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
