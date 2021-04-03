1e40382b889f4d23ba43820e46ab45de9967e00e
Your Name <you@example.com>
2021-04-03 00:23:10 -0300

    Fix

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from '../components/Form/Input';
 
 type SignInFormData = {
@@ -7,8 +9,15 @@ type SignInFormData = {
   password:string;
 }
 
+const signInFormSchema = yup.object({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState, errors } = useForm();
+  const { register, handleSubmit, formState, errors } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (data, event) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -38,14 +47,14 @@ export default function SignIn() {
             name="email" 
             label="E-mail" 
             error={errors.email}
-            ref={register({ required: 'E-mail obrigatório' })} 
+            ref={register} 
           />
           <Input 
             type="password" 
             name="password" 
             label="Senha" 
             error={errors.password}
-            ref={register({ required: 'Senha obrigatório' })} 
+            ref={register} 
           />
         </Stack>       
         <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>
