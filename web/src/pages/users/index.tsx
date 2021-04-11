048db6eb3410fc3d5aebf091ade0b177d5299978
Your Name <you@example.com>
2021-04-11 11:16:49 -0300

    Ix

--- web/src/pages/users/index.tsx
+++ web/src/pages/users/index.tsx
@@ -43,6 +43,8 @@ export default function UserList() {
             };
         });
         return users;
+    }, {
+        staleTime: 1000 * 5, // 5 segundos
     });
 
     console.log('data', data);
